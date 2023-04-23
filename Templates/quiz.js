e43b8e41a3e6b9b209650fc95e114737857edf13
CJT1981 <66756816+CJT1981@users.noreply.github.com>
2023-04-23 12:35:03 -0700

    Quiz JavaScript

--- Templates/quiz.js
+++ Templates/quiz.js
@@ -1,1 +1,10 @@
-
+/*
+<script>
+    function submitted(e){
+        e.preventDefault();
+        
+        let namee = documenr.forms["id"]["name"].value;
+        sessionStorage.setItem("name", namee);
+    }
+</script>
+*/
